e063549a8851da27c6e9fa2b88a3c3f4db5cfd95
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-27 18:45:24 +0800

    fixup! init nix-darwin config

--- flake.nix
+++ flake.nix
@@ -18,18 +18,41 @@
         { pkgs, ... }:
         {
           ids.gids.nixbld = 30000;
-          programs.fish.enable = true;
+
+          users = {
+            knownUsers = [ "fanshi" ];
+            users.fanshi = {
+              uid = 503;
+              shell = pkgs.fish;
+            };
+          };
+
+          programs = {
+            fish.enable = true;
+            direnv.enable = true;
+          };
+
+          fonts.packages = with pkgs; [
+            nerd-fonts.symbols-only
+            sarasa-gothic
+          ];
 
           # List packages installed in system profile. To search by name, run:
           # $ nix-env -qaP | grep wget
-          environment.systemPackages = [
+          environment.systemPackages = with pkgs; [
+            cachix
+            git
+            libvterm-neovim
+            emacs-pgtk
+            localsend
+            nix-output-monitor
+            ripgrep
+            yt-dlp
+            zstd
           ];
 
           # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
-
-          # Enable alternative shell support in nix-darwin.
-          # programs.fish.enable = true;
 
           # Set Git commit hash for darwin-version.
           system.configurationRevision = self.rev or self.dirtyRev or null;
