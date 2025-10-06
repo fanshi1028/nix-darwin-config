0c0fb40f0c03cebaf2d6d5a36db265cfb3216d57
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-10-06 16:50:55 +0800

    remove unused comments

--- flake.nix
+++ flake.nix
@@ -37,8 +37,6 @@
             sarasa-gothic
           ];
 
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
           environment.systemPackages = with pkgs; [
             cachix
             git
