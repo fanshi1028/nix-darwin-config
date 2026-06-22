e6ddf6225f8168472002fc6d311b6af3367acdc8
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2026-06-22 09:05:22 +0800

    dir-local nix compile command

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nix-mode . ((projectile-project-compilation-cmd . "sudo darwin-rebuild switch"))))
