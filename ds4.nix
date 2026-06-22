7f36f561416adbea2dd74517e1a5af491dd3e794
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2026-06-22 09:05:22 +0800

    fix ds4 metal sources

--- ds4.nix
+++ ds4.nix
@@ -31,7 +31,25 @@ stdenv.mkDerivation {
     for bin in ds4 ds4-server ds4-bench ds4-eval ds4-agent; do
       install -m 755 "$bin" "$out/bin/"
       wrapProgram "$out/bin/$bin" \
-        --set DS4_METAL_FLASH_ATTN_SOURCE "$out/include/metal/flash_attn.metal"
+        --set DS4_METAL_FLASH_ATTN_SOURCE "$out/include/metal/flash_attn.metal" \
+        --set DS4_METAL_DENSE_SOURCE "$out/include/metal/dense.metal" \
+        --set DS4_METAL_MOE_SOURCE "$out/include/metal/moe.metal" \
+        --set DS4_METAL_DSV4_HC_SOURCE "$out/include/metal/dsv4_hc.metal" \
+        --set DS4_METAL_UNARY_SOURCE "$out/include/metal/unary.metal" \
+        --set DS4_METAL_DSV4_KV_SOURCE "$out/include/metal/dsv4_kv.metal" \
+        --set DS4_METAL_DSV4_ROPE_SOURCE "$out/include/metal/dsv4_rope.metal"   \
+        --set DS4_METAL_DSV4_MISC_SOURCE "$out/include/metal/dsv4_misc.metal"   \
+        --set DS4_METAL_ARGSORT_SOURCE "$out/include/metal/argsort.metal" \
+        --set DS4_METAL_CPY_SOURCE "$out/include/metal/cpy.metal" \
+        --set DS4_METAL_CONCAT_SOURCE "$out/include/metal/concat.metal" \
+        --set DS4_METAL_GET_ROWS_SOURCE "$out/include/metal/get_rows.metal" \
+        --set DS4_METAL_SUM_ROWS_SOURCE "$out/include/metal/sum_rows.metal" \
+        --set DS4_METAL_SOFTMAX_SOURCE "$out/include/metal/softmax.metal" \
+        --set DS4_METAL_REPEAT_SOURCE "$out/include/metal/repeat.metal" \
+        --set DS4_METAL_GLU_SOURCE "$out/include/metal/glu.metal" \
+        --set DS4_METAL_NORM_SOURCE "$out/include/metal/norm.metal" \
+        --set DS4_METAL_BIN_SOURCE "$out/include/metal/bin.metal" \
+        --set DS4_METAL_SET_ROWS_SOURCE "$out/include/metal/set_rows.metal"
     done
     runHook postInstall
   '';
